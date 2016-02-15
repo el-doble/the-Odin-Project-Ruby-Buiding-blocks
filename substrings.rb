d7cd9d0399a7d76d73f6e966e936e8c1da899aed
Joe Doble <joedoble@fsmail.net>
2016-02-15 12:35:20 +0000

    complete substrings

---
+++ substrings.rb
@@ -0,0 +1,13 @@
+def substrings(word, substrings)
+	search_results = {}
+	substrings.each do |substring|
+		if word.downcase.match(substring)
+			search_results[substring] = word.downcase.scan(substring).count
+		end
+	end
+	return search_results
+end
+
+dictionary = ["below", "down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
